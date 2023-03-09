e0827bbc3d706000964ff2c4ce36886e9a4c629c
Axel LAMBERT <lbrt.axel@gmail.com>
2023-03-09 11:13:28 +0100

    Show mouse when inventory is open

--- bonus/main.c
+++ bonus/main.c
@@ -6,7 +6,7 @@
 /*   By: axlamber <axlamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 15:07:00 by axlamber          #+#    #+#             */
-/*   Updated: 2023/03/08 11:33:47 by axlamber         ###   ########.fr       */
+/*   Updated: 2023/03/09 11:12:29 by axlamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,32 @@
 
 int	mouse_hook(int x, int y, void *param)
 {
-	t_game	*game;
-	int		diff_x;
+	static int	hide = 0;
+	t_game		*game;
+	int			diff_x;
 
 	game = (t_game *)param;
 	(void)y;
 	diff_x = x - game->mouse.x;
-	if (game->mouse.x > x)
-		vec_rotate_edit(&(game->player.direction), diff_x / 50);
-	else if (game->mouse.x < x)
-		vec_rotate_edit(&(game->player.direction), diff_x / 50);
-	game->mouse.x = RES_X / 2;
-	mlx_mouse_move(_mlx()->mlx, _mlx()->win, RES_X / 2, RES_Y / 2);
+	if (!game->key_states['e'])
+	{
+		if (!hide)
+		{
+			mlx_mouse_hide(_mlx()->mlx, _mlx()->win);
+			hide = 1;
+		}
+		if (game->mouse.x > x) 
+			vec_rotate_edit(&(game->player.direction), diff_x / 50);
+		else if (game->mouse.x < x)
+			vec_rotate_edit(&(game->player.direction), diff_x / 50);
+		game->mouse.x = RES_X / 2;
+		mlx_mouse_move(_mlx()->mlx, _mlx()->win, RES_X / 2, RES_Y / 2);
+	}
+	else if (hide)
+	{
+		mlx_mouse_show(_mlx()->mlx, _mlx()->win);
+		hide = 0;
+	}
 	return (0);
 }
 
@@ -40,7 +54,6 @@ int	main(int argc, char **argv)
 	var_init(&game);
 	usleep(1000);
 	ma_device_start(&game.sounds.footstep.device);
-	mlx_mouse_hide(_mlx()->mlx, _mlx()->win);
 	mlx_hook(_mlx()->win, 2, 1L << 1, handle_keypress, &game);
 	mlx_hook(_mlx()->win, 3, 1L << 0, handle_keyrelease, &game);
 	mlx_hook(_mlx()->win, 6, 1L << 6, mouse_hook, &game);

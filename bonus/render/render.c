0df2992521b1ff64689341b7b86ff8ec4f0a01e1
Rukkyaa <lbrt.axel@gmail.com>
2023-03-23 13:38:59 +0100

    Restore FPS + print fps function

--- bonus/render/render.c
+++ bonus/render/render.c
@@ -6,7 +6,7 @@
 /*   By: rukkyaa <rukkyaa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 14:03:13 by axlamber          #+#    #+#             */
-/*   Updated: 2023/03/23 13:13:18 by rukkyaa          ###   ########.fr       */
+/*   Updated: 2023/03/23 13:36:23 by rukkyaa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,10 @@
 
 void	render_ui(t_game *game)
 {
-	char	*fps;
-
 	put_img_to_img( &game->hud.aim, &game->fps_img, game->camera.half_res.x - game->hud.aim.width / 2, game->camera.half_res.y - game->hud.aim.width / 2);
 	update_animation(game , &game->hud.weapon_anim);
 	//put_img_to_img( &game->hud.weapon, &game->fps_img, RES_X -  game->hud.weapon.width - RES_X / 2  , RES_Y -  game->hud.weapon.heigth);
 	put_img_to_img( game->hud.weapon_anim.current_img, &game->fps_img, RES_X/2 -  game->hud.weapon_anim.current_img->width/2  , RES_Y -  game->hud.weapon_anim.current_img->heigth);
-	fps = ft_itoa(game->time.fps);
-	mlx_string_put(game->mlx, game->fps_win, 100 , RES_Y - 20, WHITE_PIXEL, fps);
-	free(fps);
 	if (game->key_states['e'])
 	{
 		put_img_to_img(&game->inventory.img, &game->fps_img, 300, 159);

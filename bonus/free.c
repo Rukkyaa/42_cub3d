752c15c1576266612546fc4cc1b80a2c6b5afb00
Rukkyaa <lbrt.axel@gmail.com>
2023-03-23 13:30:19 +0100

    Leaks fix

--- bonus/free.c
+++ bonus/free.c
@@ -6,7 +6,7 @@
 /*   By: rukkyaa <rukkyaa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 15:20:40 by axlamber          #+#    #+#             */
-/*   Updated: 2023/03/23 13:24:07 by rukkyaa          ###   ########.fr       */
+/*   Updated: 2023/03/23 13:29:27 by rukkyaa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,12 +56,15 @@ int	close_window(t_game *game)
 	int	i;
 
 	mlx_destroy_window(game->mlx, _mlx()->win);
-	mlx_destroy_image(game->mlx, _mlx()->img.mlx_img);
+	mlx_destroy_image(_mlx()->mlx, _mlx()->img.mlx_img);
+	mlx_destroy_image(_mlx()->mlx, game->debug_img.mlx_img);
+	mlx_destroy_image(_mlx()->mlx, game->minimap.mlx_img);
 	i = -1;
 	free_sprites(game->sprites);
 	while (game->animations.zombie_run.imgs[++i])
 	{
 		mlx_destroy_image(_mlx()->mlx, game->animations.zombie_run.imgs[i]->mlx_img);
+		free(game->animations.zombie_run.imgs[i]);
 		// printf("Anim : %d\n", i);
 	}
 	free(game->animations.zombie_run.imgs);
@@ -69,6 +72,7 @@ int	close_window(t_game *game)
 	while (game->hud.weapon_anim.imgs[++i])
 	{
 		mlx_destroy_image(_mlx()->mlx, game->hud.weapon_anim.imgs[i]->mlx_img);
+		free(game->hud.weapon_anim.imgs[i]);
 		// printf("Anim : %d\n", i);
 	}
 	free(game->hud.weapon_anim.imgs);

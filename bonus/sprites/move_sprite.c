2f91ab0ca423bfabc05fe9322bd2785d438bc367
Axel LAMBERT <lbrt.axel@gmail.com>
2023-04-04 14:06:13 +0200

    Clean the move sprite

--- bonus/sprites/move_sprite.c
+++ bonus/sprites/move_sprite.c
@@ -6,11 +6,22 @@
 /*   By: axlamber <axlamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 15:36:07 by axlamber          #+#    #+#             */
-/*   Updated: 2023/04/04 14:01:38 by axlamber         ###   ########.fr       */
+/*   Updated: 2023/04/04 14:05:51 by axlamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
+
+static void	move_mob(t_sprite *sprite, t_player *player)
+{
+	if (vec_distance(sprite->pos, player->pos) < 30
+		|| vec_distance(sprite->pos, player->pos) > 400)
+		return ;
+	sprite->speed = vec_sum(player->pos, vec_scalar_mult(sprite->pos, -1));
+	sprite->speed = vec_normalize(sprite->speed);
+	sprite->pos = vec_sum(vec_scalar_mult(sprite->speed, sprite->velocity), sprite->pos);
+	sprite->pos.z = -5;
+}
 
 void	move_sprites(t_sprite *sprites, t_player *player)
 {
@@ -19,16 +30,8 @@ void	move_sprites(t_sprite *sprites, t_player *player)
 	begin = sprites;
 	while (sprites)
 	{
-		if (sprites->type != MOB || vec_distance(sprites->pos, player->pos)
-			< 30 || vec_distance(sprites->pos, player->pos) > 400)
-		{
-			sprites = sprites->next;
-			continue;
-		}
-		sprites->speed = vec_sum(player->pos, vec_scalar_mult(sprites->pos, -1));
-		sprites->speed = vec_normalize(sprites->speed);
-		sprites->pos = vec_sum(vec_scalar_mult(sprites->speed, sprites->velocity), sprites->pos);
-		sprites->pos.z = -5;
+		if (sprites->type == MOB)
+			move_mob(sprites, player);
 		sprites = sprites->next;
 	}
 }

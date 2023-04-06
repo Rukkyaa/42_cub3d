50ae2dbd9847ebca22273817556de6b9d6fb3735
Axel LAMBERT <lbrt.axel@gmail.com>
2023-04-06 13:10:07 +0200

    Normed the update animation

--- bonus/render/animation.c
+++ bonus/render/animation.c
@@ -6,20 +6,22 @@
 /*   By: axlamber <axlamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 12:59:39 by theo              #+#    #+#             */
-/*   Updated: 2023/04/05 19:02:57 by axlamber         ###   ########.fr       */
+/*   Updated: 2023/04/06 13:09:26 by axlamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
 
-void    update_animation(t_game *game, t_animation *animation)
+void	update_animation(t_game *game, t_animation *animation)
 {
-    if(animation->nb_imgs == 1)
-        return ;
-    int time_elapsed_ms = (game->time.frame.tv_sec * 1000 + game->time.frame.tv_usec / 1000) - animation->start_time_ms; 
-    int img_index = (time_elapsed_ms / animation->frame_duration_ms + animation->frame_offset) % animation->nb_imgs;
-    // printf("animation_start : %ld\n", animation->start_time_ms);
-    // printf("time_elapsed_ms : %d\n", time_elapsed_ms);
-    // printf("img_index : %d\n", img_index);
-    animation->current_img = animation->imgs[img_index];
+	int	time_elapsed_ms;
+	int	img_index;
+
+	if (animation->nb_imgs == 1)
+		return ;
+	time_elapsed_ms = (game->time.frame.tv_sec * 1000
+			+ game->time.frame.tv_usec / 1000) - animation->start_time_ms;
+	img_index = (time_elapsed_ms / animation->frame_duration_ms
+			+ animation->frame_offset) % animation->nb_imgs;
+	animation->current_img = animation->imgs[img_index];
 }

c33240a92d70ef790ef49f581aa201b8760e9fe2
Theotime ELIET <teliet@e1r12p5.clusters.42paris.fr>
2023-04-24 14:50:35 +0200

    normi: render/put_img_to_img.c

--- bonus/render/psychedelic_view.c
+++ bonus/render/psychedelic_view.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   psychedelic_view.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: theo <theo@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: teliet <teliet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 13:42:46 by axlamber          #+#    #+#             */
-/*   Updated: 2023/03/10 13:59:35 by theo             ###   ########.fr       */
+/*   Updated: 2023/04/24 14:47:10 by teliet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	psychedelic_view(t_game *game, t_img *img)
 	int	i;
 	int	j;
 	int	pixel_color;
-	// printf("%f\n", ((float) (game->frame_count % 300) / 300.0f));
+
 	game->player.direction_adjust = cos(game->frame_count) + 3;
 	i = -1;
 	while (++i < RES_X)
@@ -25,7 +25,8 @@ void	psychedelic_view(t_game *game, t_img *img)
 		j = -1;
 		while (++j < RES_Y)
 		{
-			pixel_color = img_pix_read(img, i, j) + (game->frame_count % 1000) * 500;
+			pixel_color = img_pix_read(img, i, j) + (game->frame_count % 1000)
+				* 500;
 			img_pix_put(img, i, j, pixel_color);
 		}
 	}
--- bonus/render/put_img_to_img.c
+++ bonus/render/put_img_to_img.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   put_img_to_img.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: axlamber <axlamber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: teliet <teliet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 15:05:34 by axlamber          #+#    #+#             */
-/*   Updated: 2023/04/07 11:41:57 by axlamber         ###   ########.fr       */
+/*   Updated: 2023/04/24 14:47:53 by teliet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,20 @@ void	put_img_to_img(t_img *img, t_img *fill, int start_x, int start_y)
 	int				x;
 	int				y;
 	unsigned int	color;
-	char	*src_pixel;
-	char	*dest_pixel;
+	char			*src_pixel;
+	char			*dest_pixel;
 
 	src_pixel = img_get_addr(img, 0, 0);
 	dest_pixel = img_get_addr(fill, start_x, start_y);
 	y = -1;
-	while (++y < img->heigth )
+	while (++y < img->heigth)
 	{
 		x = -1;
 		while (++x < img->width)
 		{
 			color = *(unsigned int *)src_pixel;
-			//printf("addr %d %d : %p\n", x, y, pixel);
-			//color = img_pix_read(img, x, y);
 			if (color != 4278190080)
-				*(unsigned int *)dest_pixel = color ; //img_pix_put(fill, start_x + x, start_y + y, color); // 
+				*(unsigned int *)dest_pixel = color;
 			src_pixel += 4;
 			dest_pixel += 4;
 		}
@@ -40,13 +38,14 @@ void	put_img_to_img(t_img *img, t_img *fill, int start_x, int start_y)
 	}
 }
 
-void	put_img_to_img_crop(t_img *img, t_img *fill, t_vector start, t_vector end)
+void	put_img_to_img_crop(t_img *img, t_img *fill, t_vector start,
+		t_vector end)
 {
 	int				x;
 	int				y;
 	unsigned int	color;
-	char	*src_pixel;
-	char	*dest_pixel;
+	char			*src_pixel;
+	char			*dest_pixel;
 
 	src_pixel = img_get_addr(img, 0, 0);
 	dest_pixel = img_get_addr(fill, start.x, start.y);
@@ -58,7 +57,7 @@ void	put_img_to_img_crop(t_img *img, t_img *fill, t_vector start, t_vector end)
 		{
 			color = *(unsigned int *)src_pixel;
 			if (color != 4278190080)
-				*(unsigned int *)dest_pixel = color ; //img_pix_put(fill, start_x + x, start_y + y, color); // 
+				*(unsigned int *)dest_pixel = color;
 			src_pixel += 4;
 			dest_pixel += 4;
 		}
@@ -72,24 +71,22 @@ void	sample_img_to_img(t_img *dest, t_img *src, int start_x, int start_y)
 	int				x;
 	int				y;
 	unsigned int	color;
-	char	*src_pixel;
-	char	*dest_pixel;
+	char			*src_pixel;
+	char			*dest_pixel;
 
 	src_pixel = img_get_addr(src, start_x, start_y);
 	dest_pixel = img_get_addr(dest, 0, 0);
 	y = -1;
-	while (++y < dest->heigth )
+	while (++y < dest->heigth)
 	{
 		x = -1;
 		while (++x < dest->width)
 		{
 			color = *(unsigned int *)src_pixel;
-			//printf("addr %d %d : %p\n", x, y, src_pixel);
-			//color = img_pix_read(src, x, y);
 			if (color != 4278190080)
-				*(unsigned int *)dest_pixel = color ; //img_pix_put(fill, start_x + x, start_y + y, color); // 
-			src_pixel += 4 ;
-			dest_pixel += 4 ;
+				*(unsigned int *)dest_pixel = color;
+			src_pixel += 4;
+			dest_pixel += 4;
 		}
 		src_pixel += 4 * (src->width - dest->width);
 	}

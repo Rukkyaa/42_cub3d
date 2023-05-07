1bb9ed1ece5da2afa310743d9212565f7e89ae94
Rukkyaa <lbrt.axel@gmail.com>
2023-05-07 12:52:05 +0200

    parsing: fix a little bug

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,11 +6,27 @@
 /*   By: rukkyaa <rukkyaa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 14:15:58 by axlamber          #+#    #+#             */
-/*   Updated: 2023/05/07 12:45:35 by rukkyaa          ###   ########.fr       */
+/*   Updated: 2023/05/07 12:52:04 by rukkyaa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
+
+char	**parse_map(char *map_path)
+{
+	parsing->map = get_map(map_path);
+	if (!parsing->map)
+	{
+		printf("Error\nInvalid map\n");
+		return (NULL);
+	}
+	if (!is_map_valid(parsing))
+	{
+		printf("Error\nInvalid map\n");
+		free_map(parsing->map);
+		return (NULL);
+	}
+}
 
 t_parsing	*parse(char	*map_path)
 {
@@ -19,16 +35,9 @@ t_parsing	*parse(char	*map_path)
 	parsing = malloc(sizeof(t_parsing));
 	if (!parsing)
 		return (NULL);
-	parsing->map = get_map(map_path);
+	parsing->map = parse_map(map_path);
 	if (!parsing->map)
 	{
-		free(parsing);
-		return (NULL);
-	}
-	if (!is_map_valid(parsing))
-	{
-		printf("Error\nInvalid map\n");
-		free_map(parsing->map);
 		free(parsing);
 		return (NULL);
 	}

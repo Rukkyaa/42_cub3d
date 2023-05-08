5f9dcb488af609ef2d33feb85fab9a500f6d8dcf
Axel LAMBERT <lbrt.axel@gmail.com>
2023-05-08 15:22:06 +0200

    parsing: player pos set

--- mandatory/window_init.c
+++ mandatory/window_init.c
@@ -6,7 +6,7 @@
 /*   By: axlamber <axlamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 12:50:00 by axlamber          #+#    #+#             */
-/*   Updated: 2023/05/08 15:17:34 by axlamber         ###   ########.fr       */
+/*   Updated: 2023/05/08 15:21:35 by axlamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,10 +49,9 @@ void	load_img(t_game *game)
 
 void	init_player(t_game *game)
 {
-	// game->player.pos.x = game->parsing->player_pos.x * 64 + 32;
-	// game->player.pos.y = game->parsing->player_pos.y * 64 + 32;
-	game->player.pos.x = 13 * 64 + 32;
-	game->player.pos.y = 3 * 64 + 32;
+	// printf("Parsing x y: %f %f\n", game->parsing->player_pos.x, game->parsing->player_pos.y);
+	game->player.pos.x = (int)game->parsing->player_pos.x * 64 + 32;
+	game->player.pos.y = (int)game->parsing->player_pos.y * 64 + 32;
 	game->player.speed.x = 0;
 	game->player.speed.y = 0;
 	game->player.collision_pos.x = game->player.pos.x + 32;

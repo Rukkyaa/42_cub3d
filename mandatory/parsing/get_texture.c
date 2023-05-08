c2fffa620e396a14dedd088dfecafaab8390f7a9
Axel LAMBERT <lbrt.axel@gmail.com>
2023-05-08 17:07:18 +0200

    fix: bug when 2 times same texture

--- mandatory/parsing/get_texture.c
+++ mandatory/parsing/get_texture.c
@@ -6,7 +6,7 @@
 /*   By: axlamber <axlamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 15:03:50 by rukkyaa           #+#    #+#             */
-/*   Updated: 2023/05/08 14:36:01 by axlamber         ###   ########.fr       */
+/*   Updated: 2023/05/08 17:06:14 by axlamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,27 @@ bool	is_texture(char *line)
 	return (false);
 }
 
+bool	already_load(t_parsing *parsing, char *line)
+{
+	if (line[0] == 'N' && line[1] == 'O' && parsing->no)
+		return (true);
+	if (line[0] == 'S' && line[1] == 'O' && parsing->so)
+		return (true);
+	if (line[0] == 'W' && line[1] == 'E' && parsing->we)
+		return (true);
+	if (line[0] == 'E' && line[1] == 'A' && parsing->ea)
+		return (true);
+	return (false);
+}
+
 bool	fill_texture(t_parsing *parsing, char *line)
 {
 	int		start;
 	int		end;
 	char	*texture;
 
+	if (already_load(parsing, line))
+		return (false);
 	start = 1;
 	while (line[++start] && line[start] == ' ')
 		;
